ab2631193176d934935f33e340ec6a577a614092
לב לאונטייב <152332597+ZLA964@users.noreply.github.com>
2024-11-23 21:37:20 +0300

    with output to comsole

--- src/telran/memory/test/MemoryServiceTest.java
+++ src/telran/memory/test/MemoryServiceTest.java
@@ -1,4 +1,30 @@
+package telran.memory.test;
+
+import org.junit.jupiter.api.Test;
+import telran.memory.utils.MemoryService;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MemoryServiceTest {
-  
+    private int[] arr;
+
+    @Test
+    void testGetMaxAvailableMemory() {
+        int maxMemory = MemoryService.getMaxAvailableMemory();
+        boolean flag;
+        try{
+            arr = new int[maxMemory];
+            flag = true;
+        } catch (Throwable e) {
+            flag = false;
+        }
+        assertTrue(flag);
+        try{
+            arr = new int[maxMemory+1];
+            flag = true;
+        } catch (Throwable e) {
+            flag = false;
+        }
+        assertFalse(flag);
+    }
 }
--- src/telran/memory/utils/MemoryService.java
+++ src/telran/memory/utils/MemoryService.java
@@ -1,4 +1,39 @@
 package telran.memory.utils;
 
 public class MemoryService {
+
+    public static int getMaxAvailableMemory() {
+        // TODO HW
+        int count = 1;
+        int maxIndex1 = 0;
+        int maxIndex2 = Integer.MAX_VALUE;
+        int testMaxIndex;
+        System.out.println("-- start --");
+        while (true) {
+            System.out.println("count= " + count++);
+            testMaxIndex = (maxIndex2 / 2) + (maxIndex1 / 2);
+            System.out.println("m1= " + maxIndex1 + ", tI= " + testMaxIndex + ", m2= " + maxIndex2);
+            if(((maxIndex2 == testMaxIndex && testMaxIndex == maxIndex1) || (maxIndex2-maxIndex1)==1)) break;
+            try {
+
+                System.out.println("testIndex= " + testMaxIndex);
+                int[] arr = new int[testMaxIndex];
+
+ //             if ((maxIndex2 - maxIndex1) == 1) break;
+            } catch (Error e) {
+                System.out.println(e.getMessage());
+                maxIndex2 =  testMaxIndex;
+                System.out.println("maxIndex2= " + maxIndex2);
+                System.out.println("=== end catch =====");
+     //           continue;
+            }
+            System.out.println("ttIndex= " + testMaxIndex);
+            maxIndex1 = (maxIndex2 / 2) + (maxIndex1 / 2);
+  //          maxIndex1 = testMaxIndex;
+            System.out.println("maxIndex1= " + maxIndex1);
+            System.out.println("=== end No catch ===");
+        }
+        System.out.println("step: " + count + "find MaxAvailableMemory: " + testMaxIndex);
+        return testMaxIndex;
+    }
 }
